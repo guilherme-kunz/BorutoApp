3760538c957573f15cdeb4564d9b8a39da6c8e30
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2022-01-11 11:32:00 -0300

    Design Top Bar

--- app/src/main/java/com/guilhermekunz/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/guilhermekunz/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,13 @@
 package com.guilhermekunz.borutoapp.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        }
+    ) {}
 }
